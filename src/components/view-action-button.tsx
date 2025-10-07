20346abec93ed85f995a8290bccfcabb109aa26c
Nishant Kumar <nishant.9c9@gmail.com>
2025-10-08 01:56:10 +0530

    style: update styling for ViewActionButton component for improved responsiveness

--- src/components/view-action-button.tsx
+++ src/components/view-action-button.tsx
@@ -15,8 +15,8 @@ export default function ViewActionButton({ slug }: { slug: string }) {
 
   return (
     <Link to={`/whiteboard/${slug}`} onClick={handleClick}>
-      <div className="flex items-center border-1 rounded-sm py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800 transition cursor-pointer">
-        <FileTextIcon size={mobile ? 12 : 16} />
+      <div className="flex items-center border-1 rounded-sm sm:py-2.5 sm:px-2.5 lg:py-2 lg:px-4 hover:bg-gray-100 dark:hover:bg-gray-800 transition cursor-pointer">
+        <FileTextIcon size={mobile ? 10 : 16} />
         <span className="hidden sm:text-xs sm:inline ml-1">Solution</span>
       </div>
     </Link>
